e7b021d362aa769e9feabed237aad6ed699d4f42
Khant Naing Set <khantnaingset@outlook.com>
2023-11-18 16:01:09 +0700

    edited script

--- scripts/install-client-tools.sh
+++ scripts/install-client-tools.sh
@@ -24,11 +24,11 @@ rm go1.21.4.linux-amd64.tar.gz
 
 # Install cfssl
 
-sudo go install github.com/cloudflare/cfssl/cmd/cfssl@latest
+go install github.com/cloudflare/cfssl/cmd/cfssl@latest
 
 # Install cfssljson
 
-sudo go install github.com/cloudflare/cfssl/cmd/cfssljson@latest
+go install github.com/cloudflare/cfssl/cmd/cfssljson@latest
 
 # Install kubectl
 
@@ -40,7 +40,7 @@ sudo apt update
 
 sudo apt install  -y apt-transport-https ca-certificates curl
 
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg -y
 
 echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
